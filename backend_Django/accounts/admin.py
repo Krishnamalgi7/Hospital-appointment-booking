9e3a0e2872453cdc63149f5e7a749fc77e91864d
ASUS <krishnamalgi6@gmail.com>
2026-05-23 22:39:39 +0530

    feat: initialize Hospital model and implement Doctor and Patient profiles with admin site registration

--- backend_Django/accounts/admin.py
+++ backend_Django/accounts/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-from .models import User
+from .models import User, Patient, Doctor
 
 
 admin.site.register(User, UserAdmin)
+admin.site.register(Patient)
+admin.site.register(Doctor)
