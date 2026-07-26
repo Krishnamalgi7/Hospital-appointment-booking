02b9d57f29accfcbeae83279aa6abcb76253085d
Krishna Malgi <krishnamalgi6@gmail.com>
2026-07-26 16:10:23 +0530

    Remove backend_FastAPI_backup from repo, keep locally

--- backend_Django/accounts/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-
-from .models import User, Patient, Doctor
-
-
-admin.site.register(User, UserAdmin)
-admin.site.register(Patient)
-admin.site.register(Doctor)
